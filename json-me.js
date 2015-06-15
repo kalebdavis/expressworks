8a25f705ed1adc69a79e4adfb8795ce6559093bf
Kaleb Davis <kbd6024@rit.edu>
2015-06-15 10:06:19 -0400

    finish all challenges

--- json-me.js
+++ json-me.js
@@ -3,8 +3,15 @@ var express = require('express')
 var app = express()
 
 app.get('/books', function(req, res) {
-  fs.readFile(process.argv[3], 'utf8', function(err, data) {
-    res.send(JSON.parse(data))
+  fs.readFile(process.argv[3], function(err, data) {
+    if(err) return res.send(500);
+    try {
+      var d = JSON.stringify(JSON.parse(data));
+      res.send(d);
+    }
+    catch(e) {
+      res.send(500);
+    }
   })
 })
 
